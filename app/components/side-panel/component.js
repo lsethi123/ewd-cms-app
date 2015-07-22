ff1b0a29c5f626764c2f65b8daffa950a26dd8a3
Igor Preston <igorpreston@gmail.com>
2015-07-22 22:53:14 +0300

    added side-panel component

---
+++ app/components/side-panel/component.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  menu: [
+    {
+      title: 'Dashboard',
+      link: 'index',
+      icon: ''
+    },
+    {
+      title: 'CRM',
+      link: 'crm',
+      icon: ''
+    },
+    {
+      title: 'Blog',
+      link: 'blog',
+      icon: ''
+    },
+    {
+      title: 'Todos',
+      link: 'todos',
+      icon: ''
+    },
+    {
+      title: 'Settings',
+      link: 'settings',
+      icon: ''
+    }
+  ]
+});
