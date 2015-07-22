d400c8fa84544db4093e3da5d0d0243e858bee1c
Igor Preston <igorpreston@gmail.com>
2015-07-23 01:22:14 +0300

    added company module

---
+++ app/models/company.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  address: DS.attr('string'),
+  city: DS.attr('string'),
+  state: DS.attr('string'),
+  phone: DS.attr('string'),
+  email: DS.attr('string')
+});
--- app/router.js
+++ app/router.js
@@ -8,7 +8,16 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('settings');
   this.route('blog');
-  this.route('crm');
+  this.route('crm', function() {
+    this.route('companies', function() {
+      this.route('new');
+      this.route('show', { path: ':company_id' }, function() {
+        this.route('opportunities');
+        this.route('tasks');
+        this.route('employees');
+      });
+    });
+  });
   this.route('todos');
   this.route('login');
   this.route('signup');
