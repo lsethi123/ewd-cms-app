4a9be18efaf1e0e1a3e21a74d363372bc3b4f232
e-karma <djbradyla@gmail.com>
2015-07-24 22:22:43 -0400

    liquid-fire and divshot added

---
+++ app/transitions.js
@@ -0,0 +1,24 @@
+export default function () {
+  // this.transition(
+  //   this.fromRoute('index'),
+  //   this.toRoute('cms'),
+  //   this.use('toUp'),
+  //   this.reverse('toDown')
+  // );
+  // this.transition(
+  //   this.fromRoute('index'),
+  //   this.toRoute('gallery'),
+  //   this.use('explode', {
+  //     matchBy: 'data-logo-id',
+  //     use: ['flyTo', { duration } ]
+  //   }, {
+  //     use: ['toLeft', { duration } ]
+  //   }),
+  //   this.reverse('explode', {
+  //     matchBy: 'data-logo-id',
+  //     use: ['flyTo', { duration } ]
+  //   }, {
+  //     use: ['toRight', { duration } ]
+  //   })
+  // );
+}
