5a7e27587d1f77dd9034563e9621339a24a6b25b
e-karma <djbradyla@gmail.com>
2015-07-25 01:56:16 -0400

    transitions from dashboard down to settings n back

--- app/transitions.js
+++ app/transitions.js
@@ -1,10 +1,44 @@
 export default function () {
-  // this.transition(
-  //   this.fromRoute('index'),
-  //   this.toRoute('cms'),
-  //   this.use('toUp'),
-  //   this.reverse('toDown')
-  // );
+  var duration = 500;
+  this.transition(
+    this.fromRoute('login'),
+    this.toRoute('index'),
+    this.use('fade'),
+    this.reverse('fade')
+  );
+  this.transition(
+    this.fromRoute('index'),
+    this.toRoute('crm'),
+    this.use('toUp'),
+    this.reverse('toDown')
+  );
+
+  this.transition(
+    this.fromRoute('crm'),
+    this.toRoute('blog'),
+    this.use('toUp'),
+    this.reverse('toDown')
+  );
+  this.transition(
+    this.fromRoute('blog'),
+    this.toRoute('todos'),
+    this.use('toUp'),
+    this.reverse('toDown')
+  );
+  this.transition(
+    this.fromRoute('todos'),
+    this.toRoute('settings'),
+    this.use('toUp'),
+    this.reverse('toDown')
+  );
+  this.transition(
+    this.fromRoute('settings'),
+    this.toRoute('index'),
+    this.use('toDown'),
+    this.reverse('toUp')
+  );
+
+
   // this.transition(
   //   this.fromRoute('index'),
   //   this.toRoute('gallery'),
