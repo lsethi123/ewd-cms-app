78019f133494b88e9d7eb14b677c7c79a4d71ac2
Igor Preston <igorpreston@gmail.com>
2015-07-26 22:55:43 +0300

    added opportunity forecast

--- app/models/opportunity.js
+++ app/models/opportunity.js
@@ -1,9 +1,24 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   stage: DS.attr('string'),
   estimatedClose: DS.attr(),
   estimatedAmount: DS.attr('string'),
+  forecast: Ember.computed('stage', 'estimatedAmount', function() {
+    let stage = this.get('stage');
+    let estAmt = this.get('estimatedAmount');
+    switch(stage) {
+      case "Demo":
+        return estAmt * 0.10;
+      case "Evaluation":
+        return estAmt * 0.15;
+      case "Solutions Planning Call":
+        return estAmt * 0.25;
+      case "Quote":
+        return estAmt * 0.50;
+    }
+  }),
   company: DS.belongsTo('company', { async: true } )
 });
