20cdfd6d43115b38f6eeae041ad42ef181bd42af
e-karma <djbradyla@gmail.com>
2015-07-26 02:13:52 -0400

    crm/companies/index update added view/edit/delete actions

--- app/crm/companies/index/controller.js
+++ app/crm/companies/index/controller.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    edit: function(company) {
+      this.transitionToRoute('crm.companies.show', company);
+      this.controllerFor('crm.companies.show').set('isEditing', true);
+    },
+    delete: function(company) {
+      company.destroyRecord();
+      return false;
+    }
+  }
 });
