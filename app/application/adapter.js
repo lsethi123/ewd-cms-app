aa2e77752315c3147451ba1bb14674427372ddba
Igor Preston <igorpreston@gmail.com>
2015-07-31 04:46:17 +0300

    updated api host

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-  // api from which app will fetch data
-  // host: 'http://api.ewebdesign.co'
+  host: 'http://104.131.186.236'
 });
