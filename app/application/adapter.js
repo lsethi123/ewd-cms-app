7c2742941e7e0e42c8c1fa970b1e97c0f4a27cb7
Igor Preston <igorpreston@gmail.com>
2015-08-02 23:16:55 +0300

    updated adapter with api credentials

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-  host: 'http://104.131.186.236'
+  host: 'http://45.55.161.203',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Expose-Headers': 'ETag',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+    'Access-Control-Allow-Headers': 'x-requested-with, Content-Type, origin, authorization, accept, client-security-token'
+  }
 });
