4ba0ae4d7fbd9f886a76b185aec97431ba7666c6
Igor Preston <igorpreston@gmail.com>
2015-08-03 23:30:42 +0300

    updated product with computed image url

--- app/models/product.js
+++ app/models/product.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
   price: DS.attr('number'),
+  image: DS.attr('file'),
+  imageUrl: Ember.computed('image', function() {
+    return "http://api.ewebdesign.co" + this.get('image');
+  }),
   inStock: DS.attr('boolean')
 });
