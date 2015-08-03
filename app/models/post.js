b995ba5677844ea8f7eda0206a698e2e565072be
Igor Preston <igorpreston@gmail.com>
2015-08-03 23:30:28 +0300

    updated post with computed image url

--- app/models/post.js
+++ app/models/post.js
@@ -4,6 +4,10 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   subtitle: DS.attr('string'),
   body: DS.attr('string'),
+  image: DS.attr('file'),
+  imageUrl: Ember.computed('image', function() {
+    return "http://api.ewebdesign.co" + this.get('image');
+  }),
   published: DS.attr('boolean'),
   tags: DS.hasMany('tag')
 });
