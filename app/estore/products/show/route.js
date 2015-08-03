4c0cd60352650c57fa460fb08c6e46df9ab0f279
Igor Preston <igorpreston@gmail.com>
2015-08-03 23:23:28 +0300

    updated product data reassignation upon querying model

--- app/estore/products/show/route.js
+++ app/estore/products/show/route.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.findRecord('product', params.product_id);
+    let product = this.store.findRecord('product', params.product_id);
+    return product;
   }
 });
