d0cec01086d162e60180f51ca0c91f2118ef113c
Igor Preston <igorpreston@gmail.com>
2015-08-03 23:31:04 +0300

    updated user with computed avatar url property

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,12 @@ export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
+  avatar: DS.attr('file'),
+  avatarUrl: Ember.computed('avatar', function() {
+    return "http://api.ewebdesign.co" + this.get('avatar');
+  }),
   password: DS.attr('string'),
+  team: DS.belongsTo('team'),
   tasks: DS.hasMany('task'),
   fullName: Ember.computed('firstName', 'lastName', function() {
     return this.get('firstName') + ' ' + this.get('lastName');
