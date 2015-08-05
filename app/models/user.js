6f4307bd36f5a07565ef7283f5f3601749033e0a
Igor Preston <igorpreston@gmail.com>
2015-08-06 02:32:54 +0300

    fixed user model

--- app/models/user.js
+++ app/models/user.js
@@ -5,13 +5,15 @@ export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
+  /*
   avatar: DS.attr('file'),
   avatarUrl: Ember.computed('avatar', function() {
     return "http://api.ewebdesign.co" + this.get('avatar');
-  }),
+  }), */
   password: DS.attr('string'),
   team: DS.belongsTo('team', { async: true } ),
-  tasks: DS.hasMany('task'),
+  tasks: DS.hasMany('task', { async: true } ),
+  todos: DS.hasMany('todo', { async: true } ),
   fullName: Ember.computed('firstName', 'lastName', function() {
     return this.get('firstName') + ' ' + this.get('lastName');
   })
