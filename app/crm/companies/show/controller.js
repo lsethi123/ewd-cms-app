64a39c2f2f5bb01c69d3c1a7cf524e2948f2d17e
Igor Preston <igorpreston@gmail.com>
2015-08-07 00:00:28 +0300

    fixed show in companies module

--- app/crm/companies/show/controller.js
+++ app/crm/companies/show/controller.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isEditing: false,
+
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+
+    cancel: function() {
+      this.set('isEditing', false);
+    },
+
+    closeModal: function() {
+      this.set('isEditing', false);
+    },
+
+    save: function() {
+      this.set('sendingData', true);
+      this.get('model').save().then((company) => {
+        this.set('sendingData', false);
+        this.toggleProperty('isEditing');
+      });
+    }
+  }
 });
