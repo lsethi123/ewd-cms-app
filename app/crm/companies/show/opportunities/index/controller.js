2091fcde0e8ad81799c45882ef776ab8cc02e79e
Igor Preston <igorpreston@gmail.com>
2015-08-06 23:57:49 +0300

    fixed index in opportunities module

--- app/crm/companies/show/opportunities/index/controller.js
+++ app/crm/companies/show/opportunities/index/controller.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    deleteOpportunity: function(opportunity) {
+    delete: function(opportunity) {
       opportunity.destroyRecord();
       return false;
     },
 
-    editOpportunity: function(opportunity) {
+    edit: function(opportunity) {
       this.transitionToRoute('crm.companies.show.opportunities.show', opportunity);
       this.controllerFor('crm.companies.show.opportunities.show').set('isEditing', true);
     }
