22c0a0095ce36aa5232f13d020c635d7cb22177f
e-karma <djbradyla@gmail.com>
2015-08-08 21:07:12 -0400

    move blog/posts/index  controller route and template to blog/index

---
+++ app/blog/index/controller.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  actions: {
+    edit: function(post) {
+      this.controllerFor('blog.posts.show').set('isEditing', true);
+      this.transitionToRoute('blog.posts.show', post);
+    },
+
+    unpublish: function(post) {
+      post.set('published', false).save().then((post) => {
+
+      });
+    },
+
+    publish: function(post) {
+      post.set('published', true).save().then((post) => {
+
+      });
+    },
+
+    delete: function(post) {
+      post.destroyRecord();
+      return false;
+    }
+  }
+});
