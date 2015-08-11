a3485624c775b737b806d19cbb224b7f2334c87b
Igor Preston <igorpreston@gmail.com>
2015-08-12 02:20:07 +0300

    fixed product categories

--- app/estore/products/new/controller.js
+++ app/estore/products/new/controller.js
@@ -1,13 +1,46 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  selectedCategories: null,
+  imageFiles: [],
 
   actions: {
     add: function() {
       this.set('sendingData', true);
-      let file = document.getElementById('file-field').files[0];
-      this.get('model').set('image', file).save().then((product) => {
+      let files = document.getElementById('file-field').files;
+      if (files) {
+        for (let i = 0; i < files.length; i++) {
+          let file = files[i];
+          let fileRecord = this.store.createRecord('image', {
+            file: file
+          });
+          this.get('imageFiles').pushObject(fileRecord);
+        }
+      }
+      let categories = this.get('selectedCategories');
+      this.get('model').save().then((product) => {
+        this.get('imageFiles').forEach((imageFile) => {
+          imageFile.save().then((image) => {
+            let imageProduct = this.store.createRecord('image-product', {
+              image: image,
+              product: product
+            });
+            imageProduct.save();
+          });
+        });
+
+        this.get('selectedCategories').forEach((category) => {
+          this.store.findRecord('category', category).then((category) => {
+            let categoryProduct = this.store.createRecord('category-product', {
+              category: category,
+              product: product
+            });
+            categoryProduct.save();
+          });
+        });
         this.set('sendingData', false);
+        this.set('selectedCategories', null);
+        this.set('imageFiles', []);
         this.transitionToRoute('estore.products.show', product);
       });
     },
--- app/estore/products/new/route.js
+++ app/estore/products/new/route.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('product');
+  },
+
+  afterModel: function() {
+    return this.store.findAll('category');
+  },
+
+  setupController: function(controller, model) {
+    controller.setProperties({
+      model: model,
+      categories: this.store.peekAll('category')
+    });
   }
 });
