5fe72de327ff270c0c7a3c2f1008e42b94fc93e6
Igor Preston <igorpreston@gmail.com>
2015-08-11 03:21:57 +0300

    fixed pages index

--- app/pages/index/controller.js
+++ app/pages/index/controller.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    delete: function(page) {
+      page.destroyRecord();
+      return false;
+    }
+  }
 });
