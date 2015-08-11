cdb3289bcbe9706cf3a90a336e349aaac7ccffea
Igor Preston <igorpreston@gmail.com>
2015-08-12 02:19:55 +0300

    fixed posts tags

--- app/blog/posts/new/controller.js
+++ app/blog/posts/new/controller.js
@@ -1,14 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  selectedTags: null,
+
   actions: {
     add: function() {
       this.set('sendingData', true);
-      let tags = this.get('tags');
-      let tag = this.get('selectedTag');
-      let file = document.getElementById('file-field').files[0];
-      this.get('model').set('tags', tags).set('image', file).save().then((post) => {
+      let tags = this.get('selectedTags');
+      this.get('model').save().then((post) => {
+        this.get('selectedTags').forEach((tag) => {
+          this.store.findRecord('tag', tag).then((tag) => {
+            let tagPost = this.store.createRecord('tag-post', {
+              tag: tag,
+              post: post
+            });
+            tagPost.save();
+          });
+        });
         this.set('sendingData', false);
+        this.set('selectedTags', null);
         this.transitionToRoute('blog.posts.show', post);
       });
 
--- app/blog/posts/new/route.js
+++ app/blog/posts/new/route.js
@@ -5,5 +5,16 @@ export default Ember.Route.extend({
     let tags = this.store.findAll('tag');
     this.controllerFor('blog.posts.new').set('tags', tags);
     return this.store.createRecord('post');
+  },
+
+  afterModel: function() {
+    return this.store.findAll('tag');
+  },
+
+  setupController: function(controller, model) {
+    controller.setProperties({
+      model: model,
+      tags: this.store.peekAll('tag')
+    });
   }
 });
