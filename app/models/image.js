90d5eb845976eb18b22d97805ef34b48ce2e6607
Igor Preston <igorpreston@gmail.com>
2015-08-11 03:42:30 +0300

    fixed api host for image model

--- app/models/image.js
+++ app/models/image.js
@@ -4,6 +4,6 @@ import Ember from 'ember';
 export default DS.Model.extend({
   file: DS.attr('file'),
   imgUrl: Ember.computed('file', function() {
-    return 'http://localhost:3000' + this.get('file');
+    return 'http://api.ewebdesign.co' + this.get('file');
   })
 });
