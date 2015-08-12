938adbff73bc919ff64cd9551187741c6854ceeb
Igor Preston <igorpreston@gmail.com>
2015-08-12 18:13:52 +0300

    namespaced api requests to the sessions

--- app/authenticators/ewd-cms.js
+++ app/authenticators/ewd-cms.js
@@ -41,7 +41,7 @@ export default Base.extend({
 
     authenticateUserWith: function(credentials) {
       return Ember.$.ajax({
-        url: 'http://api.ewebdesign.co/sessions',
+        url: 'http://api.ewebdesign.co/api/sessions',
         type: 'POST',
         data: credentials,
         dataType: 'json',
