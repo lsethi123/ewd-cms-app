b67d2eb1cd3eced56d4d43adbb2dd764e631a3ff
Igor Preston <igorpreston@gmail.com>
2015-08-13 03:59:59 +0300

    updated dashboard

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return new Ember.RSVP.hash({
+      tasks: this.store.query('task', { user_id: this.get('session.secure.id') } ),
+      todos: this.store.query('todo', { user_id: this.get('session.secure.id' ) } ),
+      opportunities: this.store.findAll('opportunity')
+    });
+  },
+
+  setupController: function(controller, models) {
+    let opportunities = models.opportunities;
+    let tasks = models.tasks;
+    let todos = models.todos;
+
+    controller.set('opportunities', opportunities);
+    controller.set('tasks', tasks);
+    controller.set('todos', todos);
+  }
 });
