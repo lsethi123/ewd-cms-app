3780419d31ac1fc5905c04b8b5587cd9452bd990
e-karma <djbradyla@gmail.com>
2015-08-24 02:22:39 -0400

    added modal view to dashboard events and todos

--- app/components/dash-news-feed/component.js
+++ app/components/dash-news-feed/component.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tasks: null,
   todos: null,
-  events: null
+  events: null,
+  actions: {
+    showTodo: function(todo) {
+      this.transitionToRoute('todos.show', todo);
+    },
+    showTask: function(task) {
+      this.transitionToRoute('tasks.show', task);
+    },
+    showEvent: function(event) {
+      this.transitionToRoute('events.show', event);
+    }
+  }
 });
--- app/events/show/controller.js
+++ app/events/show/controller.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isEditing: false,
+
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+
+    cancelEdit: function() {
+      this.set('isEditing', false);
+    },
+
+    cancel: function() {
+      window.history.back();
+    }
+  }
 });
