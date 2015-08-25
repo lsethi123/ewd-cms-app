5d790cd6a1c569eca015bb288f0c5b0cc8e64f69
Igor Preston <igorpreston@gmail.com>
2015-08-25 22:34:23 +0300

    fixed models

--- app/models/post.js
+++ app/models/post.js
@@ -4,11 +4,7 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   subtitle: DS.attr('string'),
   body: DS.attr('string'),
-  image: DS.attr('file'),
-  imageUrl: Ember.computed('image', function() {
-    return "http://hcr.ewebdesign.co" + this.get('image');
-    // return "http://localhost:3000" + this.get('image');
-  }),
   published: DS.attr('boolean'),
-  tags: DS.hasMany('tag', { async: true } )
+  tags: DS.hasMany('tag', { async: true } ),
+  images: DS.hasMany('image', { async: true } )
 });
--- app/models/product.js
+++ app/models/product.js
@@ -5,13 +5,7 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
   price: DS.attr('number'),
-  image: DS.attr('file'),
-  imageUrl: Ember.computed('image', function() {
-    return "http://hcr.ewebdesign.co" + this.get('image');
-    // return 'http://localhost:3000' + this.get('image');
-
-
-  }),
   inStock: DS.attr('boolean'),
-  categories: DS.hasMany('category', { async: true } )
+  categories: DS.hasMany('category', { async: true } ),
+  images: DS.hasMany('image', { async: true } )
 });
