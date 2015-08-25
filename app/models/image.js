78898524700eb11d7250796cb8a32a5430231183
Igor Preston <igorpreston@gmail.com>
2015-08-25 23:51:07 +0300

    fixed image display for products

--- app/models/image.js
+++ app/models/image.js
@@ -1,10 +1,11 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default DS.Model.extend({
   file: DS.attr('file'),
   imgUrl: Ember.computed('file', function() {
-    return 'http://localhost:3000' + this.get('file');
+    return config.apiHost +  this.get('file');
     // return 'http://localhost:3000' + this.get('file');
 
   })
