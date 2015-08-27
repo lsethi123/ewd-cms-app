296ec5108864e3d8e96b31f833860b3b1ec7dd7c
Igor Preston <igorpreston@gmail.com>
2015-08-27 05:38:14 +0300

    3 layouts for blog

--- app/blog/controller.js
+++ app/blog/controller.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    showTableLayout: function() {
+      this.controllerFor('blog.index').set('isShowingDataLayout', false);
+      this.controllerFor('blog.index').set('isShowingCardLayout', false);
+      this.controllerFor('blog.index').set('isShowingTableLayout', true);
+    },
+
+    showDataLayout: function() {
+      this.controllerFor('blog.index').set('isShowingTableLayout', false);
+      this.controllerFor('blog.index').set('isShowingCardLayout', false);
+      this.controllerFor('blog.index').set('isShowingDataLayout', true);
+    },
+
+    showCardLayout: function() {
+      this.controllerFor('blog.index').set('isShowingTableLayout', false);
+      this.controllerFor('blog.index').set('isShowingDataLayout', false);
+      this.controllerFor('blog.index').set('isShowingCardLayout', true);
+    }
+  }
 });
